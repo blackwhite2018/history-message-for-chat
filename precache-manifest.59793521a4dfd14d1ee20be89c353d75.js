f982a4aca44048b5383397fa8e33d4173ed32a94
Sergei M <black_white2018@rambler.ru>
2020-06-07 23:48:57 +0300

    Updates

---
+++ precache-manifest.59793521a4dfd14d1ee20be89c353d75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9cdcc3ad27674c7ca10adb64d8d0927",
+    "url": "/history-message-for-chat/index.html"
+  },
+  {
+    "revision": "6d00a0db9cac037342df",
+    "url": "/history-message-for-chat/static/css/main.048f9788.chunk.css"
+  },
+  {
+    "revision": "3699129ed56e4434a620",
+    "url": "/history-message-for-chat/static/js/2.fc393e05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/history-message-for-chat/static/js/2.fc393e05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d00a0db9cac037342df",
+    "url": "/history-message-for-chat/static/js/main.48e84ae3.chunk.js"
+  },
+  {
+    "revision": "d09d04cd5fbeefaa3e2e",
+    "url": "/history-message-for-chat/static/js/runtime-main.1828a928.js"
+  }
+]);
